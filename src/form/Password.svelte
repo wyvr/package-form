ff5833663718d9a5bed93d861c0fbd339e8dbb8f
wyvr <info@wyvr.dev>
2024-02-09 23:57:34 +0100

    :recycle: extract revealer styling

--- src/form/Password.svelte
+++ src/form/Password.svelte
@@ -46,46 +46,20 @@
         <label for={id}><slot /></label>
     {/if}
     {#if reveal}
-        <div class="revealer" on:mousedown={show} on:mouseup={hide}>
-            {#if revealed}
-                ✘
+        <button type="button" class="revealer" on:mousedown={show} on:mouseup={hide}>
+            {#if $$slots.reveal}
+                <slot name="reveal" {revealed} />
+            {:else if revealed}
+                hide
             {:else}
-                👁
+                show
             {/if}
-        </div>
+        </button>
     {/if}
 </div>
 
 <style>
     @import '@src/form/_base.css';
     @import '@src/form/_input.css';
-
-    .revealer {
-        position: absolute;
-        right: 0;
-        bottom: 0;
-        width: calc(var(--size) * 2.5);
-        height: calc(var(--size) * 2.5);
-        padding: var(--size);
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        cursor: pointer;
-    }
-    .revealer:after {
-        position: absolute;
-        opacity: 0;
-        content: '';
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        background: radial-gradient(closest-side, var(--color-primary), transparent);
-        transition: opacity 0.1s linear;
-        z-index: 1;
-    }
-    .revealer:hover::after,
-    .revealer:active::after {
-        opacity: 0.5;
-    }
+    @import '@src/form/_revealer.css';
 </style>
